52c76e0ff8babfc8a665c2393eb6e964cbd20255
Razvan Prichici <razvanpaulp@gmail.com>
2022-01-23 23:34:38 +0200

    Add Log4j2, enable json logging in a cloud environment.

--- essentialprogramming-api/src/main/java/com/server/Server.java
+++ essentialprogramming-api/src/main/java/com/server/Server.java
@@ -3,6 +3,8 @@ package com.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.config.Log4JConfig.configureLog4j;
+
 /**
  * Entry point for SpringBootApplication
  */
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Server {
 
     public static void main(String[] args) {
+
+        configureLog4j();
         SpringApplication.run(Server.class, args);
     }
 }
